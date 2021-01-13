0a413eb948addb96dc1054d0893e514071970fe9
papiliofly <papiliofly0723@gmail.com>
2021-01-13 16:16:41 +0900

    마우스효과

--- common.js
+++ common.js
@@ -15,3 +15,19 @@ function MobileTopBar__init() {
 }
 
 MobileTopBar__init();
+
+
+const $cursor = $('.cursor');
+const $cursorSahdow = $('.cursor-shadow');
+
+$(window).mousemove(function(e) {
+  $cursor.css({
+    top:e.clientY,
+    left:e.clientX
+  });
+  
+  $cursorSahdow.css({
+    top:e.clientY,
+    left:e.clientX
+  });
+});

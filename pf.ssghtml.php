7406abda9382a439be2d4561948ca24bcb8d7353
papiliofly <papiliofly0723@gmail.com>
2021-01-17 22:10:17 +0900

    배포

---
+++ pf.ssghtml.php
@@ -0,0 +1,23 @@
+<?php
+require_once "head.php";
+?>   
+
+<link rel="stylesheet" href="css/pf.css">
+<script src="js/pf.js" defer></script>
+
+            <section class="section-title con-min-width">
+                <h1 class="con">
+                    <span>
+                      <i class="fas fa-clipboard-list"></i>
+                    </span>
+                    <span>
+                        PORTFOLIO
+                    </span>
+                </h1>
+            </section>
+
+          
+
+<?php
+require_once "foot.php";
+?>   
